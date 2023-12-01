f6619687cffb1f1a65794a0b016d275b63ddf9b2
ssaadaou <salma.saadaoui1998@gmail.com>
2023-12-01 21:10:25 +0100

    >>>

--- CP_02/ex01/Fixed.cpp
+++ CP_02/ex01/Fixed.cpp
@@ -6,11 +6,22 @@
 /*   By: ssaadaou <ssaadaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 21:08:24 by ssaadaou          #+#    #+#             */
-/*   Updated: 2023/12/01 04:34:47 by ssaadaou         ###   ########.fr       */
+/*   Updated: 2023/12/01 16:58:58 by ssaadaou         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+    fixed_point = 5498;
+    std::cout << "constructor" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "destructor" << std::endl;
+}
 
 Fixed::Fixed(const int a)
 {
@@ -24,3 +35,41 @@ Fixed::Fixed(const float b)
     this->fixed_point = roundf(b * (1 <<  this->fractional_bits));
 }
 
+Fixed::Fixed(const Fixed &other)// creates new object as a copy of an existing obj by copying value using ref 
+{
+    std::cout << "copy constructor" << std::endl;
+    *this = other;//call the operator=()
+}
+
+Fixed&  Fixed::operator=(const Fixed &other) 
+{
+    std::cout << "copy assingment operator" << std::endl;
+    if (this != &other)
+    {
+        this->fixed_point = other.fixed_point; 
+    }
+    return (*this);
+}
+
+int Fixed::toInt(void) const
+{
+    std::cout << "from fixed to int -> " << this->fixed_point << this->fractional_bits << std::endl;
+    return(this->fixed_point << this->fractional_bits);
+}
+
+float Fixed::toFloat(void) const
+{
+    
+}
+
+int main()
+{
+
+    Fixed a;
+    Fixed b( a );
+  
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << a.toInt() << std::endl;
+    
+    
+}
--- CP_02/ex01/Fixed.hpp
+++ CP_02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: ssaadaou <ssaadaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 21:08:20 by ssaadaou          #+#    #+#             */
-/*   Updated: 2023/12/01 04:33:52 by ssaadaou         ###   ########.fr       */
+/*   Updated: 2023/12/01 16:50:00 by ssaadaou         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -21,12 +21,16 @@ class Fixed
         int fixed_point;
         static const int fractional_bits = 8;
     public:
-        Fixed(const int a);// take int and It converts it to the corresponding fixed-point value
-        Fixed(const float b);// take float, It converts it to the corresponding fixed-point value.
-        ~Fixed();
+        Fixed ();
+        Fixed (const int a);// takes int and It converts it to the corresponding fixed-point value
+        Fixed (const float b);// takes float, It converts it to the corresponding fixed-point value.
         Fixed (const Fixed &other);
-        Fixed& operator=(const Fixed &other);
-        ///
+        ~Fixed ();
+        Fixed& operator= (const Fixed &other);
+        
         int getRawBits(void) const;
         void setRawBits(int const raw);
-};
+
+        int	toInt(void) const;
+	    float toFloat(void) const;
+};  
